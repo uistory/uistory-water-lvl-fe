d4ed231b85f3e947036a5bcc9b8ce8e048b657ea
Peter Mihal <petermihal.mail@gmail.com>
2023-11-20 13:26:21 +0100

    revert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,12 @@
-// import DashboardComponent from "@/app/components/Dashboard";
-// import { ToastContainer } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
+import DashboardComponent from "@/app/components/Dashboard";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-// export default function Home() {
-//   return (
-//     <>
-//       <DashboardComponent />
-//       <ToastContainer></ToastContainer>
-//     </>
-//   );
-// }
-
-import Dashboard from "@/app/components/Dashboard";
-import dynamic from "next/dynamic";
-
-const SpaRoot = dynamic(() => import("../app/components/Dashboard"), {
-  ssr: false,
-});
-
-const IndexPage = () => {
-  return <SpaRoot />;
-};
-
-export default IndexPage;
+export default function Home() {
+  return (
+    <>
+      <DashboardComponent />
+      <ToastContainer></ToastContainer>
+    </>
+  );
+}
