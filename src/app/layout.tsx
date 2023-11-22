0a6fccf1b1794b6307d8426036725a61a8952068
Peter Mihal <petermihal.mail@gmail.com>
2023-11-22 14:04:46 +0100

    label

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Level",
+  title: "Monitor Next.js",
   description: "Peter Mihal",
 };
 
