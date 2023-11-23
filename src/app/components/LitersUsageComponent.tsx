a74e8984f300aa5a79b5acec24c948551eebcc32
Peter Mihal <petermihal.mail@gmail.com>
2023-11-23 11:52:04 +0100

    layout updates

--- src/app/components/LitersUsageComponent.tsx
+++ src/app/components/LitersUsageComponent.tsx
@@ -52,7 +52,7 @@ export default function LitersUsageComponent(props: LitersUsageComponentProps) {
             </div>
           </div>
           <div className="p-4 flex items-left">
-            <div className="p-3 rounded-full icon-circle text-green-200 text-green-100 bg-green-100 bg-green-500">
+            <div className="p-3 mr-5 rounded-full icon-circle text-green-200 text-green-100 bg-green-100 bg-green-500">
               <FontAwesomeIcon
                 icon={faShower}
                 className="fa-solid faShower"
@@ -70,7 +70,7 @@ export default function LitersUsageComponent(props: LitersUsageComponentProps) {
         </div>
         <div className="min-w-0 rounded-lg shadow-xs overflow-hidden bg-white bg-gray-800">
           <div className="p-4 flex items-left">
-            <div className="p-3 rounded-full icon-circle text-orange-100 bg-orange-100 bg-orange-500">
+            <div className="p-3 mr-5 rounded-full icon-circle text-orange-100 bg-orange-100 bg-orange-500">
               <FontAwesomeIcon
                 icon={faDroplet}
                 className="fa-solid faDroplet"
@@ -88,7 +88,7 @@ export default function LitersUsageComponent(props: LitersUsageComponentProps) {
         </div>
         <div className="min-w-0 rounded-lg shadow-xs overflow-hidden bg-white bg-gray-800">
           <div className="p-4 flex items-left">
-            <div className="p-3 rounded-full icon-circle text-blue-200 dark:text-blue-100 bg-blue-100 bg-blue-500">
+            <div className="p-3 mr-5 rounded-full icon-circle text-blue-200 dark:text-blue-100 bg-blue-100 bg-blue-500">
               <FontAwesomeIcon
                 icon={faToilet}
                 className="fa-solid faToilet"
@@ -107,7 +107,7 @@ export default function LitersUsageComponent(props: LitersUsageComponentProps) {
         </div>
         <div className="min-w-0 rounded-lg shadow-xs overflow-hidden bg-white bg-gray-800">
           <div className="p-4 flex items-left">
-            <div className="p-3 rounded-full icon-circle text-teal-200 dark:text-teal-100 bg-teal-100 bg-teal-500">
+            <div className="p-3 mr-5 rounded-full icon-circle text-teal-200 dark:text-teal-100 bg-teal-100 bg-teal-500">
               <svg fill="currentColor" viewBox="0 0 20 20" className="w-5 h-5">
                 <FontAwesomeIcon
                   icon={faBath}
